d05f50275eb080ca5d92bceb45e01fbe2f3578f7
Edward Needham <edwardneedham123@gmail.com>
2020-08-01 21:15:38 +0100

    app receiving on port

--- server/dist/app.js
+++ server/dist/app.js
@@ -5,8 +5,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const helmet_1 = __importDefault(require("helmet"));
+const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const body_parser_1 = __importDefault(require("body-parser"));
 dotenv_1.default.config();
 const app = express_1.default();
 app.use(helmet_1.default());
+app.use(cors_1.default({
+    origin: process.env.CORS_ORIGIN,
+}));
+app.use(body_parser_1.default.json());
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+app.listen(process.env.PORT || 3000);
 //# sourceMappingURL=app.js.map
--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
@@ -12,4 +14,11 @@ app.use(cors({
   origin: process.env.CORS_ORIGIN,
 }));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(process.env.PORT || 3000);
