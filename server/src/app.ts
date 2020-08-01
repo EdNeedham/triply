9e4b32ea4290137cf2c05c879b0adff92feb5e35
Edward Needham <edwardneedham123@gmail.com>
2020-08-01 16:33:44 +0100

    added winston for logging

---
+++ server/src/app.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(helmet());
+
